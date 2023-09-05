be972ed29a7bb4123466e85a764dcf82da80f32f
yanad <yana.danilova1509@gmail.com>
2023-09-05 10:57:31 +0200

    Initial commit

---
+++ src/main/java/com/springboot/fluxapp/FluxappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.fluxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/fluxapp/FluxappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.fluxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
